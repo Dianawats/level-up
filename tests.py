df24d0d121f4b63838870e9f5b82726775d8b0c9
Dianawats <dianakiznaki@gmail.com>
2018-08-09 11:03:06 +0300

    adding more tests[#fixes 159624383]

--- tests.py
+++ tests.py
@@ -3,4 +3,19 @@ from sign_up import Register, CombineData
 
 
 class TestSignup(unittest.TestCase):
+    def test_full_name(self):
+        name = CombineData('Diana', 'Wats', 'diana@gmail.com',
+                           '785730287', '+256')
+        self.assertEqual(name.combine_name(), 'Diana Wats')
+
+    def test_validate_email_and_returns_email(self):
+        register = Register.validate_email('diana@gmail.com')
+        self.assertEqual(register, 'diana@gmail.com')
+
+    def test_validate_email_and_returns_error(self):
+        with self.assertRaises(ValueError):
+            Register.validate_email('diana@gmail.com')
+
     
+if __name__ == ' __main__':
+    unittest.main()
