74b61590df9299c05823a946bbdeb41f14ce4faa
Dianawats <dianakiznaki@gmail.com>
2018-08-09 10:17:00 +0300

    making changes in python file [#fixes 159591331]

--- sign_up.py
+++ sign_up.py
@@ -2,31 +2,47 @@
 import re
 
 
-class SignUp:
+class Register:
+    """Sign up class with validate phone number and email""" 
 
-    """Sign up class with validate email""" 
-    def __init__(self, first_name, last_name, email_address, 
-                 phone_number, country_code):
+    def __init__(self, first_name, last_name, country_code, 
+                 phone_number, email_address):
         self.first_name = first_name
         self.last_name = last_name
-        self.email_address = SignUp.validate_email(email_address)
-        self.phone_number = SignUp.validate_number(phone_number)
         self.country_code = country_code
+        self.phone_number = Register.validate_phone(phone_number)
+        self.email_address = Register.validate_email(email_address)    
         
-    """Combines first and lastname into a single name"""
-    def combined_name(self):
-        self.first_name = "Diana"
-        self.last_name = "Naki"
-        fullName = "first_name" + " " + "last_name"
-        print(fullName)
+    @staticmethod
+    def validate_phone(phone_number):
+                
+        rule = re.compile(r"^[0-9]{10,14}$")
+        if not rule.search(phone_number):
+            print("Invalid phone number")
+            raise ValueError("Wrong phone number")
+        else:
+            return phone_number
 
+    @staticmethod
+    def validate_email(email_address):
+        EMAIL_REGEX = re.compile(r"^[A-Za-z0-9_-]+@[A-Za-z0-9_-]+\.[a-zA-Z]*$")
+        if not EMAIL_REGEX.match(email_address):
+            print('invalid email_address')
+            raise ValueError("Wrong email_address format")
+        else:
+            print('valid email_address')
+            print(email_address)
+            return email_address
 
-class CombineForm(SignUp):
+        
+class CombineData(Register):
 
     def __init__(self, first_name, last_name, email_address, 
                  phone_number, country_code):
-        super().__init__(self, first_name, last_name, email_address, 
-                         phone_number, country_code)
+        super().__init__(self, first_name, last_name, 
+                         email_address, 
+                         phone_number, 
+                         country_code)
 
     def combine_name(self):
         full_name = '{}  {}'.format(self.first_name, self.last_name)
@@ -38,8 +54,12 @@ class CombineForm(SignUp):
         print(full_number)
         return full_number
 
-    def combine_all_data_and_save_to_file(self):
-        all_data = '{} {} {}{} {}'.format(self.first_name, self.last_name, self.country_code, self.phone_number, self.email_address)
+    def combine_all_data_and_save_it_to_file(self):
+
+        all_data = '{} {} {}{} {}'.format(self.first_name, self.last_name,
+                                          self.email_address,
+                                          self.phone_number, 
+                                          self.country_code)
         save_data = all_data + '\n'
         data = open('data.txt', 'a')
         data.write(save_data)
@@ -47,28 +67,8 @@ class CombineForm(SignUp):
         print(all_data)
         return all_data
 
-    @staticmethod
-    def validate_email(email):
-        EMAIL_REGEX = re.compile(r"^[A-Za-z0-9_-]+@[A-Za-z0-9_-]+\.[a-zA-Z]*$")
-        if not EMAIL_REGEX.match(email):
-            print('invalid email')
-            raise ValueError("Wrong email format")
-        else:
-            print('valid email')
-            print(email)
-            return email
-
-    @staticmethod
-    def combine_phone_number(self):
-        whole_number = '{}{}'.format(self.country_code, self.phone_number)
-        print(whole_number)
-        return whole_number
-
-    
-
-    SignUp.combine_phone_number()
-    name.combine_phone_number()
-    name = CombineData('diana', 'naki', '+256', '75875858', 'diand@gmail.com')
-
-    name.combine_name()
-    name.combine_all_data_and_save_to_file(
+        
+name = CombineData('Diana', 'Wats', 'diana@gmail.com', '785730287', '+256')
+name.combine_phone_number()
+name.combine_name()
+name.combine_all_data_and_save_to_file()
