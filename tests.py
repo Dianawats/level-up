725093a4c1ab55bf5237c67de4d0be55a05addb0
Dianawats <dianakiznaki@gmail.com>
2018-08-10 15:56:56 +0300

    updating the tests file[Fixes #159624383]

--- tests.py
+++ tests.py
@@ -3,38 +3,39 @@ from sign_up import Register
 
 
 class TestSignup(unittest.TestCase):
-    def test_full_name(self):
-        name = CombineData('Diana', 'Wats', 'diana@gmail.com',
-                           '785730287', '+256')
-        self.assertEqual(name.combine_name(), 'Diana Wats')
+    
+    def test_model_is_created(self):
+        register = Register('dian', 'wats', '785730287', 
+                            'diana@gmail.com', '98765')
 
-    def test_validate_email_and_returns_email(self):
-        register = Register.validate_email('diana@gmail.com')
-        self.assertEqual(register, 'diana@gmail.com')
+    # def test_validate_email_and_returns_email(self):
+    #     register = Register.validate_email('diana@gmail.com')
+    #     self.assertEqual(register, 'diana@gmail.com')
 
-    def test_validate_email_and_returns_error(self):
-        with self.assertRaises(ValueError):
-            Register.validate_email('diana@gmail.com')
+    def test_valid_email(self):
+        register = Register('dian', 'wats', '785730287', 
+                            'diana@gmail.com', '98765')
+        self.assertEqual(register.validate_email(), 'diana@gmail.com')
 
-    def test_validate_email_returns_phone_number(self):
-        phone = Register.validate_phone('785730287')
-        self.assertEqual(phone, '785730287')
+    # def test_invalid_email(self):
+    #     register = Register('dian', 'wats', '785730287', 'wats.com', '98765')
+    #     self.assertEqual(register.validate_email(), 'Invalid email address!')
 
-    def test_user_data_type(self):
-        data = Register('first_name', 'last_name')
-        user_details = {'first_name':'louis', 'email':'email@email.com', 
-                        'phone':783140203}
-        user_data = data.register_user(user_details)
-        self.assertEqual(200, user_data['status_code'])
-        self.assertEqual('valid data format', user_data['message'])
+    def test_not_combine_non_alphabet_names(self):
+        register = Register('98765', 'wats', '785730283', 
+                            'diana@gmail.com', '98765')
+        self.assertEqual(register.combine_name(), 'Names must be alphabets')
 
-    # def test_check_missing_first_name(self):
-    #     data = Register('first_name', 'last_name')
-    #     user_details = {'username':'louis', 'email':'email@email.com',
-    #                     'phone':783140203}
-    #     user_data = data.register_user(user_details)
-    #     self.assertEqual(400, user_data['status_code'])
+    def test_combines_alphabet_names(self):
+        register = Register('Dinwats', 'Wats', '785730287', 
+                            'diana@gmail.com', '98766')
+        self.assertEqual(register.combine_name(), 'Dinwats Wats')
 
-  
-if __name__ == ' __main__':
+    def test_validate_password_length(self):
+        register = Register('Dian', 'Wats', '785730287', 
+                            'dian@gmail.com', '3W#T3')
+        self.assertEqual(register.validate_password(), 'Password not exceed ten xters long')
+
+
+if __name__ == '__main__':
     unittest.main()
